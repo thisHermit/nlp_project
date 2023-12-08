e34ac992437cfb2c3a282b6a24e19198714017a9
Jonas Luehrs <jluehrs@outlook.de>
2023-12-08 16:40:12 +0100

    Improve data separation for output files

--- evaluation.py
+++ evaluation.py
@@ -271,33 +271,33 @@ def test_model_multitask(args, model, device):
 
         with open(args.sst_dev_out, "w+") as f:
             print(f"dev sentiment acc :: {dev_sentiment_accuracy :.3f}")
-            f.write(f"id \t Predicted_Sentiment \n")
+            f.write(f"id,Predicted_Sentiment\n")
             for p, s in zip(dev_sst_sent_ids, dev_sst_y_pred):
-                f.write(f"{p} , {s} \n")
+                f.write(f"{p},{s}\n")
 
         with open(args.sst_test_out, "w+") as f:
-            f.write(f"id \t Predicted_Sentiment \n")
+            f.write(f"id,Predicted_Sentiment\n")
             for p, s in zip(test_sst_sent_ids, test_sst_y_pred):
-                f.write(f"{p} , {s} \n")
+                f.write(f"{p},{s}\n")
 
         with open(args.para_dev_out, "w+") as f:
             print(f"dev paraphrase acc :: {dev_paraphrase_accuracy :.3f}")
-            f.write(f"id \t Predicted_Is_Paraphrase \n")
+            f.write(f"id,Predicted_Is_Paraphrase\n")
             for p, s in zip(dev_para_sent_ids, dev_para_y_pred):
-                f.write(f"{p} , {s} \n")
+                f.write(f"{p},{s}\n")
 
         with open(args.para_test_out, "w+") as f:
-            f.write(f"id \t Predicted_Is_Paraphrase \n")
+            f.write(f"id,Predicted_Is_Paraphrase\n")
             for p, s in zip(test_para_sent_ids, test_para_y_pred):
-                f.write(f"{p} , {s} \n")
+                f.write(f"{p},{s}\n")
 
         with open(args.sts_dev_out, "w+") as f:
             print(f"dev sts corr :: {dev_sts_corr :.3f}")
-            f.write(f"id \t Predicted_Similarity \n")
+            f.write(f"id,Predicted_Similarity\n")
             for p, s in zip(dev_sts_sent_ids, dev_sts_y_pred):
-                f.write(f"{p} , {s} \n")
+                f.write(f"{p},{s}\n")
 
         with open(args.sts_test_out, "w+") as f:
-            f.write(f"id \t Predicted_Similarity \n")
+            f.write(f"id,Predicted_Similarity\n")
             for p, s in zip(test_sts_sent_ids, test_sts_y_pred):
-                f.write(f"{p} , {s} \n")
+                f.write(f"{p},{s}\n")
