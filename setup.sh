2d469c7ab13c7e528ec2a15a27344666fd152113
Bashar Jaan Khan <basharjaankhan@gmail.com>
2024-08-26 20:38:45 +0200

    make setup faster

--- setup.sh
+++ setup.sh
@@ -5,20 +5,20 @@ set -e  # Please don't remove this line. It makes sure that the bash script
         # lines
 
 # Set up Conda, install Python
-conda create -n dnlp python=3.10
+conda create -y -n dnlp python=3.10
 conda activate dnlp
 
 # Check for CUDA and install appropriate PyTorch version
 if command -v nvidia-smi &>/dev/null; then
     echo "CUDA detected, installing PyTorch with CUDA support."
-    conda install pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 pytorch-cuda=12.1 -c pytorch -c nvidia
+    conda install -y pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 pytorch-cuda=12.1 -c pytorch -c nvidia
 else
     echo "CUDA not detected, installing CPU-only PyTorch."
-    conda install pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 cpuonly -c pytorch
+    conda install -y pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 cpuonly -c pytorch
 fi
 
 # Install additional packages
-conda install tqdm==4.66.2 requests==2.31.0 transformers==4.38.2 tensorboard==2.16.2 tokenizers==0.15.1 scikit-learn==1.5.1 -c conda-forge -c huggingface
+conda install -y tqdm==4.66.2 requests==2.31.0 transformers==4.38.2 tensorboard==2.16.2 tokenizers==0.15.1 scikit-learn==1.5.1 -c conda-forge -c huggingface
 pip install explainaboard-client==0.1.4 sacrebleu==2.4.0 optuna==3.6.1 smart_pytorch==0.0.4 sophia-optimizer==0.2.5
 python3 fix_sophia.py
 
